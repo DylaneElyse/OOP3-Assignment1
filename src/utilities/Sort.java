237732d19de8bea096184d44cb9ca5c0e9dac260
Dylane Cunningham <155758209+DylaneElyse@users.noreply.github.com>
2025-02-25 20:54:48 -0700

    Add comments to Radix Sort

--- src/utilities/Sort.java
+++ src/utilities/Sort.java
@@ -16,10 +16,6 @@ public class Sort
 
 	public static <T extends Comparable<T>> void bubbleSort(T[] shapes)
 	{
-//		int i, j;
-//		T temp;
-//		boolean swapped;
-//		for (i=o)
 		
 	}
 
@@ -76,8 +72,14 @@ public class Sort
 		
 	}
 
+	/**
+	 * This method processes through a Radix Sort. It takes a list of numbers, sorts them into buckets based off of the digit in a specific index in the number.
+	 * @param shapes represents and Array of Shapes
+	 */
 	public static void radixSort(Shape[] shapes)
 	{
+		// Creation of the buckets for sorting objects into
+		// Creation of the map for referencing buckets 
 		Map<Integer, ArrayList<Shape>> numbersMap = new TreeMap<>(Collections.reverseOrder());
 		ArrayList<Shape> zero = new ArrayList<> ();
 		numbersMap.put(0, zero);
@@ -100,84 +102,102 @@ public class Sort
 		ArrayList<Shape> nine = new ArrayList<> ();
 		numbersMap.put(9, nine);
 
+		// maxLen: a variable that holds an int declaring the max length of the variable being compared
+		// count: int variable that keeps track of how many iterations of the main loop
 		int maxLen = 0;
 		int count = 1;
-		
+
+		// loop that checks the maximum length of the compared variable, and assigns the int to maxLen
 		for( Shape s : shapes) {
 			String height = String.valueOf(s.getHeight());
 			if (height.length() > maxLen) {
 				maxLen = height.length();
 			}
 		}
-		
+
+		// index: creates a variable of the index for the position of the least significant digit for the largest length
 		int index = maxLen - 1;
 
+			// loop that will continue iterating until it has been processed as many times as the maximum number of digits
     		while(count < maxLen) {
-              			int i = 0;
- 			for(Shape s : shapes)
-			{
-				String height = String.valueOf(s.getHeight());
-				Character digit = '0';
-				if (height.length() + count > maxLen && height.length() == maxLen) {
-					int adjustedIndex = getAdjustedIndex(height, index);
-					digit = height.charAt(adjustedIndex);
-				}
-				if (height.length() + count > maxLen && height.length() < maxLen) {
-					int adjustedIndex = index - (maxLen - height.length());
-					adjustedIndex = getAdjustedIndex(height, adjustedIndex);
-					if(adjustedIndex >= 0 && count <= height.length()) {
+      			int i = 0;
+	 			for(Shape s : shapes)
+				{
+					String height = String.valueOf(s.getHeight());
+					Character digit = '0';
+					// if statement that checks if the length of the current compared String matches the maximum length
+					// if it does, it adjusts the index if necessary based on "." placement
+					if (height.length() + count > maxLen && height.length() == maxLen) {
+						int adjustedIndex = getAdjustedIndex(height, index);
 						digit = height.charAt(adjustedIndex);
 					}
+					// if statement that checks if the length of the current compared String is less than the maximum length
+					// if it is, then it adjusts the index to the correct position, based on length and the "." placement
+					if (height.length() + count > maxLen && height.length() < maxLen) {
+						int adjustedIndex = index - (maxLen - height.length());
+						adjustedIndex = getAdjustedIndex(height, adjustedIndex);
+						if(adjustedIndex >= 0 && count <= height.length()) {
+							digit = height.charAt(adjustedIndex);
+						}
+					}
+					// if neither if statement applies, the digit index remains 0 as the length of the String is too short
+					
+					// switch case to sort the object into the proper bucket based on the digit at the desired index
+					switch (digit) {
+						case '0':
+							zero.add(s);
+							break;
+						case '1':
+							one.add(s);
+							break;
+						case '2':
+							two.add(s);
+							break;
+						case '3':
+							three.add(s);
+							break;
+						case '4':
+							four.add(s);
+							break;
+						case '5':
+							five.add(s);
+							break;
+						case '6':
+							six.add(s);
+							break;
+						case '7':
+							seven.add(s);
+							break;
+						case '8':
+							eight.add(s);
+							break;
+						case '9':
+							nine.add(s);
+							break;	
+					}
+					// changes the object at the current index of the Array to null
+					shapes[i] = null;
+					i++;
+					// continues processing until the loop has sorted the each entry
 				}
-				switch (digit) {
-					case '0':
-						zero.add(s);
-						break;
-					case '1':
-						one.add(s);
-						break;
-					case '2':
-						two.add(s);
-						break;
-					case '3':
-						three.add(s);
-						break;
-					case '4':
-						four.add(s);
-						break;
-					case '5':
-						five.add(s);
-						break;
-					case '6':
-						six.add(s);
-						break;
-					case '7':
-						seven.add(s);
-						break;
-					case '8':
-						eight.add(s);
-						break;
-					case '9':
-						nine.add(s);
-						break;	
-				}
-				shapes[i] = null;
-				i++;
-			}
 
+	 		// variable that holds the index for the next position for adding objects back into the Array
 			int reAdd = 0;
 			
+			// loop to process through the dictionary/map, and readd the objects in the new order back into the Array
 			for(Map.Entry<Integer, ArrayList<Shape>> entry : numbersMap.entrySet()) {
-				Integer name = entry.getKey();
 				ArrayList<Shape> list = entry.getValue();
 				
 				for(Shape s : list) {
 					shapes[reAdd] = s;
 					reAdd++;
 				}
+				// clears the current arrayList so it is fresh for the next iteration 
 				list.clear();
 			}
+			// adds 1 to the count of iterations
 			count++;
+			// subtracts 1 from the index for the next index location
 			index--;
  		}
 	}
@@ -188,6 +208,12 @@ public class Sort
 		
 	}
 
+	/**
+	 * Method adjusts the index passed into it to ignore the "." in the String
+	 * @param numStr String being passed into the method 
+	 * @param digitIndex Desired index of the String
+	 * @return Returns the new index if it has been affected by the "."
+	 */
 	private static int getAdjustedIndex(String numStr, int digitIndex) {
 	    int adjustedIndex = digitIndex; // Start with the original index
 	    for (int i = 0; i < numStr.length(); i++) {
